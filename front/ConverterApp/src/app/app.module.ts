4652d4abcc355f591dc71030f715198158fbd91b
Rémy Melquiond <remy@XPS_Remy.localdomain>
2018-01-23 15:22:25 +0100

    Updated all packages to latest version, Material module added

--- front/ConverterApp/src/app/app.module.ts
+++ front/ConverterApp/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {MatNativeDateModule,} from '@angular/material';
+import {HttpModule} from "@angular/http";
+import {MaterialModule} from './material.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule { }
+
+export class AppModule {}
+
+//platformBrowserDynamic().bootstrapModule(AppModule);
--- front/ConverterApp/src/main.ts
+++ front/ConverterApp/src/main.ts
@@ -1,8 +1,11 @@
+import './polyfills';
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { MaterialModule } from './app/material.module';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
